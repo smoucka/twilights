c96fb3ff3c071265134baaf9026f310242b45c1e
swm <smoucka@gmail.com>
2014-08-31 18:22:07 -0500

    can process multiple files

--- process_data.py
+++ process_data.py
@@ -1,4 +1,8 @@
 import csv, struct, copy
+
+files = ['_daytime', '_civil_twilight', '_nautical_twilight', '_astronomical_twilight']
+# city name prefix in twilight files
+cities = ['miami']
 	
 # storage
 times = []
@@ -9,8 +13,7 @@ def rise_set(tp):
 def twilights(tp, day):
 	times[day].insert(0, tp[0].split(' ')[0])
 	times[day].append(tp[0].split(' ')[1])
-	
-	
+		
 def parse(iter_num, in_file):
 	day_count = 0
 	# field widths
@@ -39,14 +42,14 @@ def parse(iter_num, in_file):
 					twilights(time_pair, day_count)
 				day_count += 1
 
-files = ['rise_set', 'civil_twilight', 'nautical_twilight', 'astronomical_twilight']
+for city in cities:
+	for file_idx, file in enumerate(files):
+		city_file = city + file
+		parse(file_idx, city_file)
 
-for file_idx, file in enumerate(files):
-	parse(file_idx, file)
-
-with open('times.csv', 'wb') as csvfile:
-	csvwriter = csv.writer(csvfile)
-	csv_head = ['astro_beg', 'naut_beg', 'civ_beg', 'rise', 'set', 'civ_end', 'naut_end', 'astro_end']
-	csvwriter.writerow(csv_head)
-	for row in times:
-		csvwriter.writerow(row)
+	with open('parsed_data/' + city + '_times.csv', 'wb') as csvfile:
+		csvwriter = csv.writer(csvfile)
+		csv_head = ['astro_beg', 'naut_beg', 'civ_beg', 'rise', 'set', 'civ_end', 'naut_end', 'astro_end']
+		csvwriter.writerow(csv_head)
+		for row in times:
+			csvwriter.writerow(row)
