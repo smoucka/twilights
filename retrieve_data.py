12d67a6b5f744f19220e95aeab6089cbfcaa41a5
swm <smoucka@gmail.com>
2017-05-22 19:59:19 -0500

    update using requests

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,18 +1,45 @@
-import subprocess
+import requests, os
+from lxml import html
 
-curl = 'curl "http://aa.usno.navy.mil/cgi-bin/aa_rstablew.pl" -H "Origin: http://aa.usno.navy.mil" -H "Accept-Encoding: gzip,deflate" -H "Accept-Language: en-US,en;q=0.8" -H "User-Agent: Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36" -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Cache-Control: max-age=0" -H "Referer: http://aa.usno.navy.mil/data/docs/RS_OneYear.php" -H "Connection: keep-alive" --data "FFX=2&xxy=2014&type=&place=&xx0=-1&xx1=0&xx2=0&yy0=1&yy1=&yy2=0&zz1=&zz0=-1&ZZZ=END" --compressed'
+url = 'http://aa.usno.navy.mil/cgi-bin/aa_rstablew.pl'
+payload = {'ID':'AA',
+			'year':'2017',
+			'task':'3',
+			'place':None,
+			'lon_sign':'-1',
+			'lon_deg':'90',
+			'lon_min':None,
+			'lat_sign':None,
+			'lat_deg':None,
+			'lat_min':None,
+			'tz':'6',
+			'tz_sign':'-1'}
 
-table_type = ['0','2','3','4']
+task_array = [{'name':'day', 'num':'0'},
+				{'name':'civil', 'num':'2'},
+				{'name':'naut', 'num':'3'},
+				{'name':'astro', 'num':'4'}]
 
-for lat in range(90,-95,-5):
+for lat in range(85,-90,-10):
 	lat_val = str(abs(lat))
+	payload['lat_deg'] = lat_val
 	if lat<0:
-		name = lat_val + 'S'
-		curl = curl.replace('yy0=1', 'yy0=-1')
+		payload['lat_sign'] = '-1'
+		ns = 'S'
 	else:
-		name = lat_val + 'N'
+		payload['lat_sign'] = '1'
+		ns = 'N'
 	
-	for tab in table_type:
-		command = curl.replace('type=', 'type=' + tab).replace('place=', 'place=' + name).replace('yy1=', 'yy1=' + lat_val)
+	for task in task_array:
+		payload['task'] = task['num']
+
+		r = requests.get(url, params=payload)
+		tree = html.fromstring(r.content)
 		
-		subprocess.call(command)
+		fpath = '/path/to/dir/'
+		fname = fpath + ns + lat_val + '_' + task['name'] + '.txt'
+
+		with open(fname, 'w') as f:
+			f.write(tree.xpath('//pre/text()')[0])
+
+		print 'created file - ' + fname
